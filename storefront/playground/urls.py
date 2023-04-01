b9639070cf39cab23132fccc9a6459b4d5e74a3d
rafaelcodrea <117072978+rafaelcodrea@users.noreply.github.com>
2023-04-01 10:41:24 -0700

    Add files via upload

--- storefront/playground/urls.py
+++ storefront/playground/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+from .views import duration_chart
 #URL configuration
 urlpatterns = [
-    path('start/',views.say_hello)
+    path('start/',views.index),
+     path('duration-chart/', views.duration_chart, name='duration_chart')
 ]
--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,28 +1,53 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Dream
-from .forms import DreamForm
+import matplotlib.pyplot as plt
 
 # Create your views here.
 #request handler
 #request -> response
 
+import logging
+logger = logging.getLogger("mylogger")
 
 
+
+def duration_chart(request):
+    # get the data from the database
+    durations = Dream.objects.values_list('duration', flat=True)
+    
+    # create a histogram
+    fig, ax = plt.subplots()
+    ax.hist(durations, bins=[1, 2, 3, 4, 5, 6], align='left')
+    ax.set_xticks([1, 2, 3, 4, 5])
+    ax.set_xticklabels(['1', '2', '3', '4', '5'])
+    ax.set_xlabel('Duration')
+    ax.set_ylabel('Count')
+    ax.set_title('Duration Chart')
+
+    # convert the plot to a Django-compatible format
+    from io import BytesIO
+    import base64
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close(fig)
+    data = base64.b64encode(buf.getbuffer()).decode('ascii')
+    return render(request, 'duration_chart.html', {'data': data})
 
 def index(request):
     if request.method == 'POST':
-        form = DreamForm(request.POST)
-        if form.is_valid():
-            description = request.POST['description']
-            duration = request.POST['duration']
-            stress_level = request.POST['stress_level']
-            energy = request.POST['energy']
-            dream = Dream(description=description, duration=duration, stress_level=stress_level, energy=energy)
-            dream.save()
-            return render(request, 'dreamcatcher/thankyou.html')
-    return render(request, 'dreamcatcher/index.html')
-
+        description = request.POST['description']
+        duration = request.POST['duration']
+        stress_level = request.POST['stress_level']
+        energy = request.POST['energy']
+        
+        dream = Dream(description=description, duration=duration, stress_level=stress_level, energy=energy)
+        logger.info("Whatever to log")
+        dream.save()
+        return render(request, 'hello.html')
+    
+    # If the request is a GET request, render the index template
+    return render(request, 'hello.html')
 
 def say_hello(request):
     return render(request, 'hello.html', {'name': 'Rafael'})
